741a9fd1c69b057a6223817f074a5e84cd973eab
Tim Sweeney <mainetim@gmail.com>
2023-06-13 14:21:50 -0400

    Refactor.

--- media_mod_file.py
+++ media_mod_file.py
@@ -1,7 +1,6 @@
 import argparse
 import os
-import sys
-from typing import Any, Tuple
+from typing import Tuple
 
 import media_library as ml
 from media_library import Entries, SortPointer
@@ -9,15 +8,6 @@ from media_library import Entries, SortPointer
 gb_no_action = False
 gb_verbose = False
 gb_write_csv = False
-
-
-def exit_error(*error_data: Any) -> None:
-    for i, data in enumerate(error_data):
-        print(data, end=" ")
-        if i != len(error_data) - 1:
-            print(" : ", end=" ")
-    print("")
-    sys.exit()
 
 
 def get_args() -> argparse.Namespace:
@@ -39,11 +29,33 @@ def find_original(
     found = True
     start = 0
     while found:
-        found, fp_index = ml.check_original_size_pointers(master, sorted_pointers, int(orig_size), start)
+        found, fp_index = ml.check_pointers_to_original_size(sorted_pointers, int(orig_size), start)
         if found and (master[fp_index].name == target.name):
             return (found, fp_index)
         start = fp_index
     return (False, 0)
+
+
+def process_targets(master: list[Entries], sorted_pointers: list[SortPointer], target: list[Entries]) -> list[Entries]:
+
+    for item in target:
+        item_path = os.path.join(item.path, item.name)
+        orig_duration, orig_size = ml.file_md_tag(item_path)
+        if orig_duration == "":
+            ml.exit_error(f"{item_path} has no mp_tag. Cannot detect original file.")
+        found, orig_index = find_original(master, sorted_pointers, item, orig_size)
+        if found:
+            if gb_verbose:
+                print(f"Found original file: {os.path.join(master[orig_index].path, master[orig_index].name)}")
+        else:
+            ml.exit_error(f"Original entry for {item.name} not found.")
+
+        master[orig_index].current_duration = ml.file_duration(item_path)
+        master[orig_index].current_size = int(os.stat(item_path).st_size)
+        master[orig_index].ino = int(os.stat(item_path).st_ino)
+        if master[orig_index].csum != "":
+            master[orig_index].csum = ml.checksum(item_path)
+    return master
 
 
 def main() -> None:
@@ -63,32 +75,14 @@ def main() -> None:
     target_path = args.target_path[0]
 
     if (master := ml.read_master_file(master_input_path)) == []:
-        exit_error(f"{master_input_path} not found and is required.")
+        ml.exit_error(f"{master_input_path} not found and is required.")
 
     if os.path.exists(target_path):
         target = ml.create_file_list(target_path)
     else:
-        exit_error(f"Target not found: {target_path}")
+        ml.exit_error(f"Target not found: {target_path}")
 
-    sorted_pointers = ml.pointer_sort_database(master)
-
-    for item in target:
-        item_path = os.path.join(item.path, item.name)
-        orig_duration, orig_size = ml.file_md_tag(item_path)
-        if orig_duration == "":
-            exit_error(f"{item_path} has no mp_tag. Cannot detect original file.")
-        found, orig_index = find_original(master, sorted_pointers, item, orig_size)
-        if found:
-            if gb_verbose:
-                print(f"Found original file: {os.path.join(master[orig_index].path, master[orig_index].name)}")
-        else:
-            exit_error(f"Original entry for {item.name} not found.")
-
-        master[orig_index].current_duration = ml.file_duration(item_path)
-        master[orig_index].current_size = int(os.stat(item_path).st_size)
-        master[orig_index].ino = int(os.stat(item_path).st_ino)
-        if master[orig_index].csum != "":
-            master[orig_index].csum = ml.checksum(item_path)
+    master = process_targets(master, ml.pointer_sort_database(master, "original_size"), target)
 
     if args.write_file:
         master.sort(key=lambda x: getattr(x, "current_size"))
